d9c0c2274b5ccefbbec055025782cf180c0e52f8
Your Name <youremail@yourdomain.com>
2021-08-25 14:11:27 -0400

    Form component positioning resolved

--- client/src/components/AddCharacter.jsx
+++ client/src/components/AddCharacter.jsx
@@ -3,7 +3,7 @@ import { baseURL, config } from "../services";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { useHistory } from "react-router";
-import "./AddCharacter.css"
+import "./AddCharacter.css";
 
 function AddCharacter(props) {
   const [name, setName] = useState("");
@@ -26,65 +26,85 @@ function AddCharacter(props) {
       weaknesses,
       location,
       image: "https://i.imgur.com/FZyBQ1S.png",
-      spray: "https://i.imgur.com/2IFjKrJ.png"
+      spray: "https://i.imgur.com/2IFjKrJ.png",
     };
     history.push("/");
     await axios.post(baseURL, { fields: newCharacter }, config);
     // }
     props.setToggleFetch((prevToggleFetch) => !prevToggleFetch);
   };
-  
 
   return (
-    <form className="formBox" onSubmit={handleSubmit}>
+    <div>
       <h1 className="FormTitle">CREATE NEW CHARACTER</h1>
-      <label htmlFor="name">Name:</label>
-      <input
-        id="name"
-        type="text"
-        onChange={(e) => setName(e.target.value)}
-        value={name}
-      />
-      <label htmlFor="type">Type:</label>
-      <input
-        type="text"
-        id="type"
-        onChange={(e) => setType(e.target.value)}
-        value={type}
-      />
-      <label htmlFor="role">Role:</label>
-      <input
-        type="text"
-        id="role"
-        onChange={(e) => setRole(e.target.value)}
-        value={role}
-      />
-      <label htmlFor="location">Location:</label>
-      <input
-        type="text"
-        id="location"
-        onChange={(e) => setLocation(e.target.value)}
-        value={location}
-      />
-      <label htmlFor="strengths">Strengths:</label>
-      <textarea
-        id="strengths"
-        onChange={(e) => setStregths(e.target.value)}
-        value={strengths}
-      />
-      <label htmlFor="weaknesses">Weaknesses:</label>
-      <textarea
-        id="weaknesses"
-        onChange={(e) => setWeaknesses(e.target.value)}
-        value={weaknesses}
-      />
-      <img
-        onClick={handleSubmit}
-        className="crack"
-        src="https://i.imgur.com/1Va0dQb.png"
-        alt="Cracked egg"
-      />
-    </form>
+
+      <form className="formBox" onSubmit={handleSubmit}>
+        {/* <label htmlFor="name">Name:</label> */}
+        <input
+          className="inputColor"
+          placeholder="New character's name"
+          id="name"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+          value={name}
+        />
+        <br />
+        {/* <label htmlFor="type">Type:</label> */}
+        <input
+          className="inputColor"
+          placeholder="Type"
+          type="text"
+          id="type"
+          onChange={(e) => setType(e.target.value)}
+          value={type}
+        />
+        <br />
+        {/* <label htmlFor="role">Role:</label> */}
+        <input
+          className="inputColor"
+          placeholder="Hero or Villain"
+          type="text"
+          id="role"
+          onChange={(e) => setRole(e.target.value)}
+          value={role}
+        />
+        <br />
+        {/* <label htmlFor="location">Location:</label> */}
+        <input
+          className="inputColor"
+          placeholder="Location"
+          type="text"
+          id="location"
+          onChange={(e) => setLocation(e.target.value)}
+          value={location}
+        />
+        <br />
+        {/* <label htmlFor="strengths">Strengths:</label> */}
+        <textarea
+          className="inputColor"
+          placeholder="Character's Strengths"
+          id="strengths"
+          onChange={(e) => setStregths(e.target.value)}
+          value={strengths}
+        />
+        <br />
+        {/* <label htmlFor="weaknesses">Weaknesses:</label> */}
+        <textarea
+          className="inputColor"
+          placeholder="Character's Weaknesses"
+          id="weaknesses"
+          onChange={(e) => setWeaknesses(e.target.value)}
+          value={weaknesses}
+        />
+        <br />
+        <img
+          onClick={handleSubmit}
+          className="crack"
+          src="https://i.imgur.com/1Va0dQb.png"
+          alt="Cracked egg"
+        />
+      </form>
+    </div>
   );
 }
 export default AddCharacter;
--- client/src/components/CharacterDetails.jsx
+++ client/src/components/CharacterDetails.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { baseURL, config } from "../services";
 import { useParams, useHistory } from "react-router-dom";
 import axios from "axios";
-import "./CharacterDetails.css"
+import "./CharacterDetails.css";
 import { motion } from "framer-motion";
-
 
 function CharacterDetails(props) {
   const { id } = useParams();
@@ -13,55 +12,66 @@ function CharacterDetails(props) {
   const character = props.characters.find((character) => character.id === id);
   console.log(character);
 
-
-
   const handleDelete = async () => {
     await axios.delete(`${baseURL}/${character.id}`, config);
     props.setToggleFetch((prevToggleFetch) => !prevToggleFetch);
-    history.push("/")
+    history.push("/");
   };
-  
 
   return (
     <div className="details-container">
       <div id="name-image">
-      
-      <motion.div
-        initial={{ scale: 0 }}
-        animate={{ scale: 1 }}
-        transition={{
-          type: "spring",
-          stiffness: 230,
-          damping: 100,
-        }}
-      >
-        <img
-        class="character-image"
-        id="image"
-        src={character.fields.image}
-        alt=""
-        />
-      </motion.div>
+        <motion.div
+          initial={{ scale: 0 }}
+          animate={{ scale: 1 }}
+          transition={{
+            type: "spring",
+            stiffness: 230,
+            damping: 100,
+          }}
+        >
+          <img
+            class="character-image"
+            id="image"
+            src={character.fields.image}
+            alt=""
+          />
+        </motion.div>
 
-      <h1 className="detailsName">{character.fields.name}</h1>
+        <h1 className="detailsName">{character.fields.name}</h1>
       </div>
       <div className="detailsFields">
-        <p>Type:
-          {character.fields.type}</p>
-        <p>Role:
-          {character.fields.role}</p>
-        <p>Strengths:
-          {character.fields.strengths}</p>
-        <p>Weaknesses:
-          {character.fields.weaknesses}</p>
-        <p>Location:
-          {character.fields.location}</p>
-        {character.fields.mainCharacter !== "true" ?
+        <p>
+          Type:
+          {character.fields.type}
+        </p>
+        <br />
+        <p>
+          Role:
+          {character.fields.role}
+        </p>
+        <br />
+        <p>
+          Strengths:
+          {character.fields.strengths}
+        </p>
+        <br />
+        <p>
+          Weaknesses:
+          {character.fields.weaknesses}
+        </p>
+        <br />
+        <p>
+          Location:
+          {character.fields.location}
+        </p>
+        {character.fields.mainCharacter !== "true" ? (
           <img
-          className="trashCan"
-          src="https://i.imgur.com/EArYeyj.png"
-          onClick={handleDelete}/>
-         : null}
+            className="trashCan"
+            src="https://i.imgur.com/EArYeyj.png"
+            onClick={handleDelete}
+          />
+        ) : null}
       </div>
     </div>
   );
